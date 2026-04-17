ab4aea64a1f3700e5811de054c9a0f9db28d0c64
gavhu10 <186771458+gavhu10@users.noreply.github.com>
2026-04-17 07:31:44 -0400

    chore: remove tests

--- tests/start-up-and-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eu
-
-/usr/sbin/pcscd &
-sleep 2
-rm -f memory.flash
-cp -R tests/docker/fido2/* /usr/local/lib/python3.11/dist-packages/fido2/hid
-./build_in_docker/pico_fido > /dev/null &
-pytest tests
